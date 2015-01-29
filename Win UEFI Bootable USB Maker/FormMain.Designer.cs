e5ef6705f91e094fd7961ed2f0aa16b9dbc2d62d
haydarsahin34 <haydarsahin34@gmail.com>
2015-01-28 16:23:44 -0800

    Files Uploaded

---
+++ Win UEFI Bootable USB Maker/FormMain.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Win_UEFİ_Bootable_USB_Maker
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroLabel_Devices = new MetroFramework.Controls.MetroLabel();
+            this.metroComboBox_Devices = new MetroFramework.Controls.MetroComboBox();
+            this.metroLabel_SourceFolder = new MetroFramework.Controls.MetroLabel();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.textBox_Browse = new System.Windows.Forms.TextBox();
+            this.metroButton_BrowseFolder = new MetroFramework.Controls.MetroButton();
+            this.metroCheckBox_Accept = new MetroFramework.Controls.MetroCheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroRadioButton_Folder = new MetroFramework.Controls.MetroRadioButton();
+            this.metroRadioButton_İmage = new MetroFramework.Controls.MetroRadioButton();
+            this.metroButton_Burn = new MetroFramework.Controls.MetroButton();
+            this.metroLabel_Status = new MetroFramework.Controls.MetroLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // metroLabel_Devices
+            // 
+            this.metroLabel_Devices.AutoSize = true;
+            this.metroLabel_Devices.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.metroLabel_Devices.Location = new System.Drawing.Point(27, 106);
+            this.metroLabel_Devices.Name = "metroLabel_Devices";
+            this.metroLabel_Devices.Size = new System.Drawing.Size(106, 19);
+            this.metroLabel_Devices.Style = MetroFramework.MetroColorStyle.Pink;
+            this.metroLabel_Devices.TabIndex = 0;
+            this.metroLabel_Devices.Text = "Device            :";
+            // 
+            // metroComboBox_Devices
+            // 
+            this.metroComboBox_Devices.FormattingEnabled = true;
+            this.metroComboBox_Devices.ItemHeight = 23;
+            this.metroComboBox_Devices.Location = new System.Drawing.Point(155, 100);
+            this.metroComboBox_Devices.Name = "metroComboBox_Devices";
+            this.metroComboBox_Devices.Size = new System.Drawing.Size(160, 29);
+            this.metroComboBox_Devices.TabIndex = 1;
+            this.metroComboBox_Devices.UseSelectable = true;
+            this.metroComboBox_Devices.SelectedIndexChanged += new System.EventHandler(this.metroComboBox_Devices_SelectedIndexChanged);
+            // 
+            // metroLabel_SourceFolder
+            // 
+            this.metroLabel_SourceFolder.AutoSize = true;
+            this.metroLabel_SourceFolder.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.metroLabel_SourceFolder.Location = new System.Drawing.Point(27, 209);
+            this.metroLabel_SourceFolder.Name = "metroLabel_SourceFolder";
+            this.metroLabel_SourceFolder.Size = new System.Drawing.Size(103, 19);
+            this.metroLabel_SourceFolder.Style = MetroFramework.MetroColorStyle.Pink;
+            this.metroLabel_SourceFolder.TabIndex = 0;
+            this.metroLabel_SourceFolder.Text = "Source           :";
+            // 
+            // textBox_Browse
+            // 
+            this.textBox_Browse.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox_Browse.Location = new System.Drawing.Point(155, 204);
+            this.textBox_Browse.Name = "textBox_Browse";
+            this.textBox_Browse.Size = new System.Drawing.Size(160, 29);
+            this.textBox_Browse.TabIndex = 2;
+            // 
+            // metroButton_BrowseFolder
+            // 
+            this.metroButton_BrowseFolder.Location = new System.Drawing.Point(335, 204);
+            this.metroButton_BrowseFolder.Name = "metroButton_BrowseFolder";
+            this.metroButton_BrowseFolder.Size = new System.Drawing.Size(111, 29);
+            this.metroButton_BrowseFolder.TabIndex = 4;
+            this.metroButton_BrowseFolder.Text = "Browse...";
+            this.metroButton_BrowseFolder.UseSelectable = true;
+            this.metroButton_BrowseFolder.Click += new System.EventHandler(this.metroButton_BrowseFolder_Click);
+            // 
+            // metroCheckBox_Accept
+            // 
+            this.metroCheckBox_Accept.AutoSize = true;
+            this.metroCheckBox_Accept.Checked = true;
+            this.metroCheckBox_Accept.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.metroCheckBox_Accept.Location = new System.Drawing.Point(155, 297);
+            this.metroCheckBox_Accept.Name = "metroCheckBox_Accept";
+            this.metroCheckBox_Accept.Size = new System.Drawing.Size(191, 15);
+            this.metroCheckBox_Accept.TabIndex = 6;
+            this.metroCheckBox_Accept.Text = "I accept to erase all files on disk ";
+            this.metroCheckBox_Accept.UseSelectable = true;
+            this.metroCheckBox_Accept.CheckedChanged += new System.EventHandler(this.metroCheckBox_Accept_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Win_UEFİ_Bootable_USB_Maker.Properties.Resources.refresh_icon;
+            this.pictureBox1.Location = new System.Drawing.Point(335, 94);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(37, 39);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.metroLabel1.Location = new System.Drawing.Point(23, 161);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(252, 38);
+            this.metroLabel1.TabIndex = 8;
+            this.metroLabel1.Text = "Take  Windows İnstall Source Files From\r\n";
+            // 
+            // metroRadioButton_Folder
+            // 
+            this.metroRadioButton_Folder.AutoSize = true;
+            this.metroRadioButton_Folder.Location = new System.Drawing.Point(281, 163);
+            this.metroRadioButton_Folder.Name = "metroRadioButton_Folder";
+            this.metroRadioButton_Folder.Size = new System.Drawing.Size(94, 15);
+            this.metroRadioButton_Folder.TabIndex = 10;
+            this.metroRadioButton_Folder.Text = "Folder / Drive";
+            this.metroRadioButton_Folder.UseSelectable = true;
+            // 
+            // metroRadioButton_İmage
+            // 
+            this.metroRadioButton_İmage.AutoSize = true;
+            this.metroRadioButton_İmage.Checked = true;
+            this.metroRadioButton_İmage.Location = new System.Drawing.Point(390, 163);
+            this.metroRadioButton_İmage.Name = "metroRadioButton_İmage";
+            this.metroRadioButton_İmage.Size = new System.Drawing.Size(77, 15);
+            this.metroRadioButton_İmage.TabIndex = 11;
+            this.metroRadioButton_İmage.TabStop = true;
+            this.metroRadioButton_İmage.Text = "İmage File";
+            this.metroRadioButton_İmage.UseSelectable = true;
+            // 
+            // metroButton_Burn
+            // 
+            this.metroButton_Burn.Location = new System.Drawing.Point(178, 380);
+            this.metroButton_Burn.Name = "metroButton_Burn";
+            this.metroButton_Burn.Size = new System.Drawing.Size(125, 35);
+            this.metroButton_Burn.TabIndex = 13;
+            this.metroButton_Burn.Text = "Burn";
+            this.metroButton_Burn.UseSelectable = true;
+            this.metroButton_Burn.Click += new System.EventHandler(this.metroButton_Burn_Click);
+            // 
+            // metroLabel_Status
+            // 
+            this.metroLabel_Status.AutoSize = true;
+            this.metroLabel_Status.Location = new System.Drawing.Point(178, 349);
+            this.metroLabel_Status.Name = "metroLabel_Status";
+            this.metroLabel_Status.Size = new System.Drawing.Size(102, 19);
+            this.metroLabel_Status.TabIndex = 15;
+            this.metroLabel_Status.Text = "Status : Waiting ";
+            // 
+            // FormMain
+            // 
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BorderStyle = MetroFramework.Forms.MetroFormBorderStyle.FixedSingle;
+            this.ClientSize = new System.Drawing.Size(490, 438);
+            this.Controls.Add(this.metroLabel_Status);
+            this.Controls.Add(this.metroButton_Burn);
+            this.Controls.Add(this.metroRadioButton_İmage);
+            this.Controls.Add(this.metroRadioButton_Folder);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.metroCheckBox_Accept);
+            this.Controls.Add(this.metroButton_BrowseFolder);
+            this.Controls.Add(this.textBox_Browse);
+            this.Controls.Add(this.metroTextBox1);
+            this.Controls.Add(this.metroComboBox_Devices);
+            this.Controls.Add(this.metroLabel_SourceFolder);
+            this.Controls.Add(this.metroLabel_Devices);
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(456, 337);
+            this.Name = "FormMain";
+            this.Text = "Windows UEFİ Bootable USB Maker";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroLabel metroLabel_Devices;
+        private MetroFramework.Controls.MetroComboBox metroComboBox_Devices;
+        private MetroFramework.Controls.MetroLabel metroLabel_SourceFolder;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private System.Windows.Forms.TextBox textBox_Browse;
+        private MetroFramework.Controls.MetroButton metroButton_BrowseFolder;
+        private MetroFramework.Controls.MetroCheckBox metroCheckBox_Accept;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroRadioButton metroRadioButton_Folder;
+        private MetroFramework.Controls.MetroRadioButton metroRadioButton_İmage;
+        private MetroFramework.Controls.MetroButton metroButton_Burn;
+        private MetroFramework.Controls.MetroLabel metroLabel_Status;
+    }
+}
+
---
+++ Win UEFI Bootable USB Maker/FormMain.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MetroFramework;
+using System.IO;
+using System.Diagnostics;
+using System.Management;
+using System.Management.Instrumentation;
+namespace Win_UEFİ_Bootable_USB_Maker
+{
+    public partial class FormMain : MetroFramework.Forms.MetroForm
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+        string path = "";
+       void list_RemovableDevices()
+        {
+            metroComboBox_Devices.Items.Clear();
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (var drive in drives)
+            {
+                if (drive.DriveType == DriveType.Removable && drive.IsReady == true)
+                {  
+                    metroComboBox_Devices.Items.Add(drive.Name+drive.VolumeLabel);
+                    path = drive.Name;
+                }
+            }
+           
+        }
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            list_RemovableDevices();
+    
+        }
+
+        private void metroButton_BrowseFolder_Click(object sender, EventArgs e)
+        {
+
+            if (metroRadioButton_İmage.Checked == true)
+            {
+                OpenFileDialog file = new OpenFileDialog();
+                file.Filter = "İmage File |*.iso";
+                file.InitialDirectory = Environment.SpecialFolder.Desktop.ToString();
+                file.Multiselect = false;
+                if (file.ShowDialog() == DialogResult.OK)
+                {
+                    textBox_Browse.Text = file.FileName;
+                }
+
+             }
+            else
+            {
+                FolderBrowserDialog folder = new FolderBrowserDialog();
+                folder.SelectedPath = Environment.SpecialFolder.Desktop.ToString();
+                if (folder.ShowDialog()==DialogResult.OK)
+                {
+                    textBox_Browse.Text = folder.SelectedPath;
+                }
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            list_RemovableDevices();
+        }
+
+        private void metroButton_Burn_Click(object sender, EventArgs e)
+        {
+            string error = "You must fill in the fields below:\n";
+            try
+            {
+                if (metroComboBox_Devices.SelectedItem.ToString()=="")
+                {
+                    error += "\n -Device";
+                }
+            }
+            catch (NullReferenceException)//Check NullReference exception error
+            {
+                error += "\n -Device";
+            }
+            finally
+            {
+                if (textBox_Browse.Text == "")
+                {
+                    error += "\n -Source";
+                }
+                if (error != "You must fill in the fields below:\n")//İF there is error 
+                {
+                    MetroMessageBox.Show(this, error, "Error", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    Process p = new Process();
+                    p.StartInfo.FileName = Environment.SystemDirectory + @"\diskpart.exe";
+                    p.StartInfo.UseShellExecute = false;                         
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.RedirectStandardInput = true;
+                    metroLabel_Status.Text = "Status : Starting";
+                    p.Start();
+                    metroLabel_Status.Text = "Status : Selecting disk";
+                    MessageBox.Show("Select disk " );
+                    p.StandardInput.WriteLine("Select disk ");
+                    metroLabel_Status.Text = "Status : Cleaning";
+                    p.StandardInput.WriteLine("clean");
+                    metroLabel_Status.Text = "Status : Creating partition";
+                    p.StandardInput.WriteLine("create partition primary");
+                    metroLabel_Status.Text = "Status : Formatting";
+                    p.StandardInput.WriteLine("format quick fs=fat32");
+                    p.StandardInput.WriteLine("active");
+                    metroLabel_Status.Text = "Status : Activating";
+                    p.StandardInput.WriteLine("assign");
+                    metroLabel_Status.Text = "Status : Assigning";
+                    p.StandardInput.WriteLine("exit");
+                    string output = p.StandardOutput.ReadToEnd();                 // Places the output to a variable
+                    p.WaitForExit();
+                    MessageBox.Show(output);
+                }
+            }     
+        }
+
+        private void metroCheckBox_Accept_CheckedChanged(object sender, EventArgs e)
+        {
+            if (metroCheckBox_Accept.Checked == true)
+            {
+                metroButton_Burn.Enabled = true;
+            }
+            else
+            {
+                metroButton_Burn.Enabled = false;
+            }
+        }
+
+        private void metroComboBox_Devices_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                ManagementObjectSearcher searcher =
+                    new ManagementObjectSearcher("root\\CIMV2",
+                    "SELECT * FROM Win32_DiskPartition.serialnumber=(SELECT * FROM win32_logicaldisk.deviceid=\"" + path + "");
+
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    MessageBox.Show(queryObj["Type"].ToString());
+                    if (queryObj["Name"].ToString()==metroComboBox_Devices.Text)
+                    {
+                        MessageBox.Show("no:" + (int.Parse(queryObj["DiskIndex"].ToString())));
+                    }
+                   
+                }
+            }
+            catch (ManagementException err)
+            {
+                MessageBox.Show("An error occurred while querying for WMI data: " + err.Message);
+            }
+
+        }
+    }
+}
